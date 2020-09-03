04500ce48462a0587280f9166c5c9d75f8102275
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-03 15:43:14 +0900

    [Add]いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about"
   resources :users
   resources :books do
+    resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
