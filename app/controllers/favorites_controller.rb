eab459e1dab135325bc37ac2ea9285eff723c0ba
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-10 15:17:07 +0900

    [Fix]不要なコード削除・部分テンプレート化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -12,7 +12,6 @@ class FavoritesController < ApplicationController
   	favorite = current_user.favorites.find_by(book_id: @book.id)
   	favorite.destroy
   	redirect_back(fallback_location: root_path)
-    redirect_back(fallback_location: root_path)
   end
 
 end
